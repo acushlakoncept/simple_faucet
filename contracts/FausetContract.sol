dc56907967111f65f23b8d03ec45b86599713fa7
Uduak Essien <acushla4real@gmail.com>
2021-12-20 01:16:34 +0100

    Add faucet contract

---
+++ contracts/FausetContract.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+    // storage variables
+    uint public funds = 1000;  // positive values only
+    int public counter = -10;
+    uint32 public test = 4294967295;
+}
