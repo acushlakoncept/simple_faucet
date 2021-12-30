abb0368d8e24d8ddf4b753e7540b60bf86851e54
Uduak Essien <acushla4real@gmail.com>
2021-12-30 12:13:51 +0100

    Add useEffect method to request connect user account

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
+import Web3 from "web3";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  });
+
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+     let provider = null;
+
+     if(window.ethereum) {
+       provider = window.ethereum;
+       try {
+         await provider.request({method: "eth_requestAccounts"});
+       } catch {
+         console.error("User denied account access");
+       }
+     } else if (window.web3) {
+       provider = window.web3.currentProvider;
+     } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider('http://localhost:7545');
+     }
+
+     setWeb3Api({
+       web3: new Web3(provider),
+       provider
+     })
+    }
+
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    }
+
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3]);
+
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
+          <span>
+            <strong>Account: </strong>
+          </span>
+          <h1>
+            { account ? account : 'not connected'}
+          </h1>
           <div className="balance-view is-size-2">
             Current Balance: <strong>10</strong> ETH
           </div>
